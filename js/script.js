d0c25788044ec03556a7fb72052271c20e7a541e
Shnekker <107854001+Shnekker@users.noreply.github.com>
2022-11-11 11:03:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.slider-reviews', {
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.s-button-next',
+		prevEl: '.s-button-prev',
+	},
+	autoplay: {
+		delay: 3000,
+	},
+});
+
